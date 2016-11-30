52a75910f623e7db642b9c7212374b3923010be7
lancedolan <lance.dolan@gmail.com>
2016-11-30 12:55:11 -0800

    Class Resource properly mapped

--- src/main/java/com/edlio/sis/rest/service/ClassResource.java
+++ src/main/java/com/edlio/sis/rest/service/ClassResource.java
@@ -4,18 +4,23 @@
 package com.edlio.sis.rest.service;
 
 import java.util.logging.Logger;
+import javax.ws.rs.GET;
 import javax.ws.rs.Path;
 import javax.ws.rs.PathParam;
+import javax.ws.rs.Produces;
+import javax.ws.rs.core.MediaType;
 
 /**
  *
  * @author lance
  */
-@Path("class")
+@Path("/class")
 public class ClassResource {
     
     private static final Logger LOG = Logger.getLogger(ClassResource.class.getName());
     
+    @GET
+    @Produces(MediaType.APPLICATION_JSON)
     @Path("{id}")
     public String getClass( @PathParam("id") final Long id) {
         LOG.info("Getting class with id " + id);
